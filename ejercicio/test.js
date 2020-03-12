e2f3b09407604e0c209c159f25a0dc16ca304197
Daniela Capponi <capponi.daniela@gmail.com>
2020-03-11 22:07:51 -0300

    subo ejercicio en clase

--- ejercicio/test.js
+++ ejercicio/test.js
@@ -1,5 +1,6 @@
 
 
+
 describe("getBoxVolume()", (size) => {
     it("Debería devolver el volúmen", () => {
 
@@ -53,14 +54,14 @@ describe("merge()", (info1, info2) => {
         expect(resultado).to.be.an("object");
 
     })
-    it("Debería devolver un objeto con { a: 1, b: 2, c: 3, d: 4, b: 5}", () => {
+    it("Debería devolver un objeto con { a: 1, c: 3, d: 4, b: 5}", () => {
 
         const info1 = { a: 1, b: 2, c: 3 }
         const info2 = { d: 4, b: 5 }
 
         const resultado = merge(info1, info2);
 
-        expect(resultado).to.eql({ a: 1, b: 2, c: 3, d: 4, b: 5 });
+        expect(resultado).to.eql({ a: 1, c: 3, d: 4, b: 5 });
 
     })
 
@@ -137,4 +138,102 @@ describe("filterProperties()", (data, props) => {
         expect(resultado).to.eql({ b: 2, c: 3 });
 
     })
+})
+
+describe("buyProducts()", (money, products) => {
+    it("Debería devolver un objeto", () => {
+
+        const products = { cookies: 60, chocolate: 110, soda: 120, }
+        const money = 115
+
+        const resultado = buyProducts(money, products);
+
+        expect(resultado).to.be.an("object");
+
+    })
+
+    it("Debería devolver un objecto cuyas values sean booleanos", () => {
+
+        const products = { cookies: 60, chocolate: 110, soda: 120, }
+        const money = 115
+
+        const resultado = buyProducts(money, products);
+
+        expect(resultado).to.eql({ cookies: true, chocolate: true, soda: false });
+
+    })
+
+})
+
+describe("canBuyAllProduct()", (money, products) => {
+    it("Debería devolver un booleano", () => {
+
+        const products = { cookies: 60, chocolate: 110, soda: 120, }
+        const money = 115
+
+        const resultado = canBuyAllProduct(money, products);
+
+        expect(resultado).to.be.a("boolean");
+
+    })
+
+    it("Debería devolver false si el dinero no alcanza", () => {
+
+        const products = { cookies: 60, chocolate: 110, soda: 120, }
+        const money = 115
+
+        const resultado = canBuyAllProduct(money, products);
+
+        expect(resultado).to.be.false;
+
+    })
+
+})
+
+describe("getXP()", (money, products) => {
+    it("Debería devolver número", () => {
+
+        const score = { easy: 10, medium: 50, hard: 100 }
+        const challenges = { easy: 3, medium: 4, hard: 2 }
+
+        const resultado = getXP(score, challenges);
+
+        expect(resultado).to.be.finite;
+
+    })
+
+    it("Debería devolver la suma del XP", () => {
+
+        const score = { easy: 10, medium: 50, hard: 100 }
+        const challenges = { easy: 3, medium: 4, hard: 2 }
+
+        const resultado = getXP(score, challenges);
+
+        expect(resultado).to.equal(430);
+
+    })
+
+})
+
+describe("getStringInfo()", (string) => {
+    it("Debería devolver objeto", () => {
+
+        const string = "H3ll0 Wor1d";
+
+
+        const resultado = getStringInfo(string);
+
+        expect(resultado).to.be.an("object");
+
+    })
+
+    it("Debería recorrer un string y contar sus elementos", () => {
+
+        const string = "H3ll0 Wor1d";
+        const resultado = getStringInfo(string);
+
+        expect(resultado).to.eql({ letters: 7, digits: 3, spaces: 1 });
+
+    })
+
 })
